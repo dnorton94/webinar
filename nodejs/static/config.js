650ab097c117ce1c5dc54e14ba8ae2698e2b1e89
dnorton94 <dominiconorton@gmail.com>
2019-12-10 16:21:47 +0100

    config improvement

--- nodejs/static/config.js
+++ nodejs/static/config.js
@@ -1,4 +1,4 @@
 var Config = {
   requestURL: "http://localhost:3000/input",
-  firstMessage: "Guten tag! I'm Lucie the virtual assistant for the Hilton Hotel fitness center worldwide.You can schedule a personal trainer and receive a personalised workout from me but first, let's get to knwo eachother."
+  firstMessage: "Guten tag! I'm Lucie the virtual assistant for the Hilton Hotel fitness center worldwide.You can schedule a personal trainer and receive a personalised workout from me but first, let's get to know eachother."
 };
